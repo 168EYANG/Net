f7b6df3aa74a3e4f04507888dd528356ca3148bb
168EYANG <121638138+168EYANG@users.noreply.github.com>
2023-05-29 20:27:38 -0500

    Basic screens for resume upload screen
    - Note: need to implement camera and file upload features later (maybe use a link to a file instead of the file itself? not sure how we would store their uploaded file)

---
+++ app/src/main/java/com/example/net/ui/login/ResumeUpload.java
@@ -0,0 +1,16 @@
+package com.example.net.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.net.R;
+
+public class ResumeUpload extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resume_upload);
+    }
+}
